068f62a69c68b2475b553e8cfdbaf33aad95d3f0
Charleschua <charleschua19@gmail.com>
2022-06-21 13:27:25 +0800

    update database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,22 @@
-const parse = require('pg-connection-string').parse;
-    const config = parse(process.env.DATABASE_URL);
-    module.exports = ({ env }) => ({
-      connection: {
-        connector: 'bookshelf',
-        client: 'postgres',
-        connection: {
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          user: config.user,
-          password: config.password,
-          ssl: {
-            rejectUnauthorized: false
-          },
-        },
-        debug: false,
+const parse = require("pg-connection-string").parse;
+
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    connector: "bookshelf",
+    client: "postgres",
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false,
       },
-    });
+    },
+    debug: false,
+  },
+});
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,4 @@
 module.exports = ({ env }) => ({
- host: env('HOST','0.0.0.0'),
+ //host: env('HOST','0.0.0.0'),
+ url: env("https://whispering-atoll-93697.herokuapp.com"),
 });
