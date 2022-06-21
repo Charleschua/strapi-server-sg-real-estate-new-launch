b173b0100d64af58bfed27c0f022703d393f9bc0
Charleschua <charleschua19@gmail.com>
2022-06-21 15:49:33 +0800

    configured pg databases for Heroku

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-module.exports = ({ env }) => ({
+/* module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
@@ -12,4 +12,53 @@ module.exports = ({ env }) => ({
       },
     },
   },
+});
+ */
+
+const parse =
+  process.env.NODE_ENV === "development"
+    ? () => ""
+    : require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+const devConnections = (env) => {
+  return {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+  };
+};
+
+const productionConnections = (env) => {
+  return {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  };
+};
+
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections:
+    env("NODE_ENV") === "development"
+      ? devConnections(env)
+      : productionConnections(env),
 });
